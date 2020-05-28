383148faf20505077b934ed7e42ed3d2ffd0c930
c0de4un <code4un@yandex.ru>
2020-05-28 23:16:14 +0400

    (feat) glm + opengl support

---
+++ cpp/cmake/libs.android.cmake
@@ -0,0 +1,50 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+#
+# btEngine Dependencies
+#
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+
+set ( BIN_DIR "${CMAKE_SOURCE_DIR}/bin" )
+set ( LIBS_DIR "${CMAKE_SOURCE_DIR}/libs" )
+
+# - - - - - - - - - - - - - - - - - GLM - - - - - - - - - - - - - - - - - - - - - -
+
+set ( GLM_LIB_DIR "${LIBS_DIR}/glm" )
+include_directories ( GLM_LIB_DIR )
+
+# - - - - - - - - - - - - - - - - - Android API  - - - - - - - - - - - - - - - - - -
+
+# Add Android Log Library
+find_library( log-lib log )
+
+# - - - - - - - - - - - - - - - - OpenGL - - - - - - - - - - - - - - - - - - - - - -
+
+if ( BT_GL_VERSION_MAJOR EQUAL 2 )
+    find_library( OpenGL GLESv2 )
+elseif ( BT_GL_VERSION_MAJOR EQUAL 3 )
+    find_library( OpenGL GLESv3 )
+else ( BT_GL_VERSION_MAJOR EQUAL 2 )
+    message ( FATAL_ERROR "${PROJECT_NAME} - OpenGL library version selector configuration required." )
+endif ( BT_GL_VERSION_MAJOR EQUAL 2 )
+
+if ( NOT EXISTS ${OpenGL} )
+    message ( STATUS "${PROJECT_NAME} - OpenGL ES library not found !" )
+endif ( NOT EXISTS ${OpenGL} )
+
+if ( BT_CMAKE_DEBUG )
+    message ( STATUS "${PROJECT_NAME} - OpenGL ES library found at ${OpenGL}" )
+endif ( BT_CMAKE_DEBUG )
+
+# - - - - - - - - - - - - - - - - OpenSL - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - SOIL - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - zlib - - - - - - - - - - - - - - - - - - - - - - -
+
+# - - - - - - - - - - - - - - - - - btEngine - - - - - - - - - - - - - - - - - - - -
+
+# btEngine
+set ( BT_CMAKE_DEBUG ON )
+add_definitions ( -DBT_DEBUG=1 )
+add_definitions ( -DDEBUG=1 )
+add_subdirectory ( "engine/btSDK" )
